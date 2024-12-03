bdf6b7beb50639c76759d14e65faf92cecc66fa2
anchalDev159357 <singhanchal159357@gmail.com>
2024-12-03 21:23:15 +0530

    add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
