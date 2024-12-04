f67a2aec69f7334e8bf930a32b1105462f973603
anchalDev159357 <singhanchal159357@gmail.com>
2024-12-04 10:20:44 +0530

    add

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -6,7 +6,16 @@ const Register: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  const gradientTextStyle: React.CSSProperties & {
+    WebkitBackgroundClip?: string;
+    WebkitTextFillColor?: string;
+  } = {
+    backgroundImage:
+      "linear-gradient(to right, #e6d802, #19c051,#485061,#63cdf6,#f24b03,#494547,#da3fa6,#40275a,#681e26)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+    textAlign: "center",
+  };
   const registerUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Prevent default form behavior
 
@@ -36,125 +45,142 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        height: "100vh",
-        textAlign: "center",
-      }}
-    >
+    <div>
       <div
         style={{
-          background: "#e0e5ec",
-          borderRadius: "12px",
-          padding: "15px 20px",
-          width: "450px",
-          height: "400px",
-          fontSize: "16px",
-          color: "#333",
-          border: "none",
-          outline: "none",
-          boxShadow:
-            "8px 8px 15px rgba(0, 0, 0, 0.1), -8px -8px 15px rgba(255, 255, 255, 0.7)",
+          backgroundImage:
+            "linear-gradient(to right, #e6d802, #19c051,#485061,#63cdf6,#f24b03,#494547,#da3fa6,#40275a,#681e26)",
+          width: "100%",
+          height: "10px",
+        }}
+      ></div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+          textAlign: "center",
         }}
       >
-        <h1>Join Us</h1>
-        <form
-          onSubmit={registerUser} // Call the registerUser function on form submission
+        <div
           style={{
+            background: "#e0e5ec",
+            borderRadius: "12px",
+            padding: "15px 20px",
+            width: "450px",
+            height: "400px",
+            fontSize: "16px",
+            color: "#333",
+            border: "none",
+            outline: "none",
+            boxShadow:
+              "8px 8px 15px rgba(0, 0, 0, 0.1), -8px -8px 15px rgba(255, 255, 255, 0.7)",
+            position: "relative",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
             flexDirection: "column",
+            marginTop: "-20px",
           }}
         >
-          <input
+          <h1>Join Us</h1>
+          <form
+            onSubmit={registerUser} // Call the registerUser function on form submission
             style={{
-              width: "300px",
-              height: "40px",
-              borderRadius: "50px",
-              background: "#fff",
-              color: "#333",
-              border: "none",
-              outline: "none",
-              boxShadow:
-                " inset 5px 5px 10px #b8bec7, inset -5px -5px 10px #ffffff",
-              paddingLeft: "10px",
-              marginTop: "10px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
             }}
-            type="text"
-            name="Username"
-            placeholder="Username"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <input
-            style={{
-              width: "300px",
-              height: "40px",
-              borderRadius: "50px",
-              background: "#fff",
-              color: "#333",
-              border: "none",
-              outline: "none",
-              boxShadow:
-                " inset 5px 5px 10px #b8bec7, inset -5px -5px 10px #ffffff",
-              paddingLeft: "10px",
-              marginTop: "20px",
-            }}
-            type="email"
-            name="Email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            style={{
-              width: "300px",
-              height: "40px",
-              borderRadius: "50px",
-              background: "#fff",
-              color: "#333",
-              border: "none",
-              outline: "none",
-              boxShadow:
-                " inset 5px 5px 10px #b8bec7, inset -5px -5px 10px #ffffff",
-              paddingLeft: "10px",
-              marginTop: "20px",
-            }}
-            type="password"
-            name="Password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button
-            style={{
-              width: "100px",
-              height: "30px",
-              borderRadius: "2px",
-              background: "#333",
-              color: "#fff",
-              border: "none",
-              outline: "none",
-              boxShadow:
-                "8px 8px 15px rgba(0, 0, 0, 0.1), -8px -8px 15px rgba(255, 255, 255, 0.7)",
-              marginTop: "20px",
-            }}
-            type="submit"
           >
-            Register
-          </button>
-        </form>
-        <p>
-          Already have an account? <a href="/login">Login</a>
-        </p>
+            <input
+              style={{
+                width: "300px",
+                height: "40px",
+                borderRadius: "50px",
+                background: "#fff",
+                color: "#333",
+                border: "none",
+                outline: "none",
+                boxShadow:
+                  " inset 5px 5px 10px #b8bec7, inset -5px -5px 10px #ffffff",
+                paddingLeft: "10px",
+                marginTop: "10px",
+              }}
+              type="text"
+              name="Username"
+              placeholder="Username"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <input
+              style={{
+                width: "300px",
+                height: "40px",
+                borderRadius: "50px",
+                background: "#fff",
+                color: "#333",
+                border: "none",
+                outline: "none",
+                boxShadow:
+                  " inset 5px 5px 10px #b8bec7, inset -5px -5px 10px #ffffff",
+                paddingLeft: "10px",
+                marginTop: "20px",
+              }}
+              type="email"
+              name="Email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              style={{
+                width: "300px",
+                height: "40px",
+                borderRadius: "50px",
+                background: "#fff",
+                color: "#333",
+                border: "none",
+                outline: "none",
+                boxShadow:
+                  " inset 5px 5px 10px #b8bec7, inset -5px -5px 10px #ffffff",
+                paddingLeft: "10px",
+                marginTop: "20px",
+              }}
+              type="password"
+              name="Password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              style={{
+                width: "100px",
+                height: "30px",
+                borderRadius: "2px",
+                background: "#333",
+                color: "#fff",
+                border: "none",
+                outline: "none",
+                boxShadow:
+                  "8px 8px 15px rgba(0, 0, 0, 0.1), -8px -8px 15px rgba(255, 255, 255, 0.7)",
+                marginTop: "20px",
+                cursor: "pointer",
+              }}
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+          <p style={{ position: "absolute", bottom: "0" }}>
+            Already have an account? <a style={gradientTextStyle} href="/login">Login</a>
+          </p>
+        </div>
       </div>
     </div>
   );
